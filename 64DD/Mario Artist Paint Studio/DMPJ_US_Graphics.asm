e690f5c0750ff6e2193313cc8fa9b9a7799adee1
luigiblood <luigiblood@gmail.com>
2016-10-08 02:36:26 +0200

    [Paint Studio] Replaced Yes/No small buttons

--- 64DD/Mario Artist Paint Studio/DMPJ_US_Graphics.asm
+++ 64DD/Mario Artist Paint Studio/DMPJ_US_Graphics.asm
@@ -3,3 +3,9 @@ seek(0x2C75B0)
 insert "GFX/BACKCIRCLE.rgba16.bin"
 seek(0x31A680)
 insert "GFX/BACKCIRCLE.rgba16.bin"
+
+//Yes / No buttons
+seek(0x2C6830)
+insert "GFX/YES_NO.rgba16.bin"
+seek(0x319900)
+insert "GFX/YES_NO.rgba16.bin"
