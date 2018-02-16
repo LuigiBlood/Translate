644f2aac681091df392d704ead088434c8e0fd6d
LuigiBlood <luigiblood@gmail.com>
2018-02-16 15:50:28 +0100

    [Paint Studio] Translate All Graphics except Main Menu

--- 64DD/Mario Artist Paint Studio/DMPJ_US_Graphics.asm
+++ 64DD/Mario Artist Paint Studio/DMPJ_US_Graphics.asm
@@ -1,3 +1,79 @@
+//--Title Screen
+seek(0xD39B90) //Paint Studio
+insert "GFX/PAINTSTUDIO.ia8.bin"
+
+seek(0xD40FD0) //Mario Artist
+insert "GFX/MARIOARTIST.ia8.bin"
+
+
+//--Buttons
+seek(0x34E640)
+insert "GFX/BUTTONS.rgba16.bin"
+
+
+//--Animation
+//How to animate
+seek(0x7290E8)
+insert "GFX/HOW_TO_ANIM.ia16.bin"
+
+//Film Frame
+seek(0x72A498)
+insert "GFX/FRAME.rgba16.bin"
+
+
+//--3D World
+//Photo
+seek(0x32FA18)
+insert "GFX/PHOTO_EXCELLENT.ia8.bin"
+
+seek(0x331E18)
+insert "GFX/PHOTO_GOOD.ia8.bin"
+
+seek(0x334218)
+insert "GFX/PHOTO_ALMOST.ia8.bin"
+
+seek(0x336618)
+insert "GFX/PHOTO_POOR.ia8.bin"
+
+
+//--Game Boy Camera
+seek(0x3232A0)
+insert "GFX/GBCAMERA_SHOOT.rgba16.bin"
+
+seek(0x323D30)
+insert "GFX/GBCAMERA_BACK.rgba16.bin"
+
+seek(0x324DC0)
+insert "GFX/GBCAMERA_PREVNEXT.rgba16.bin"
+
+
+//--Capture Cartridge
+seek(0xD8E290)
+insert "GFX/CAPTURE_BRIGHT.ia16.bin"
+
+seek(0xD8E488)
+insert "GFX/CAPTURE_CONTRAST.ia16.bin"
+
+seek(0xD8E758)
+insert "GFX/CAPTURE_HUE.ia16.bin"
+
+seek(0xD8E950)
+insert "GFX/CAPTURE_SHOOT.rgba16.bin"
+
+seek(0xD8F3E0)
+insert "GFX/CAPTURE_EXIT.rgba16.bin"
+
+
+//--Save/Load
+seek(0x2AECD0)
+insert "GFX/SAVELOAD.i8.bin"
+
+seek(0x2D9BB0) //Load Color
+insert "GFX/LOADSELECT.ci8.bin"
+
+seek(0x2DA3F0) //Save Color
+insert "GFX/SAVESELECT.ci8.bin"
+
 //Back Circle Arrow
 seek(0x2C75B0)
 insert "GFX/BACKCIRCLE.rgba16.bin"
@@ -9,24 +85,3 @@ seek(0x2C6830)
 insert "GFX/YES_NO.rgba16.bin"
 seek(0x319900)
 insert "GFX/YES_NO.rgba16.bin"
-
-//Film Frame
-seek(0x72A498)
-insert "GFX/FRAME.rgba16.bin"
-
-//Save/Load
-seek(0x2AECD0)
-insert "GFX/SAVELOAD.i8.bin"
-
-seek(0x2D9BB0) //Load Color
-insert "GFX/LOADSELECT.ci8.bin"
-
-seek(0x2DA3F0) //Save Color
-insert "GFX/SAVESELECT.ci8.bin"
-
-//Title Screen
-seek(0xD39B90) //Paint Studio
-insert "GFX/PAINTSTUDIO.ia8.bin"
-
-seek(0xD40FD0) //Mario Artist
-insert "GFX/MARIOARTIST.ia8.bin"
