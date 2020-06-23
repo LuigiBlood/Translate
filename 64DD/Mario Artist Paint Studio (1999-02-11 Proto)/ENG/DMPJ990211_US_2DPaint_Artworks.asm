0c2f7fd7fa7cbfffc1d234df2ad5b8becbd95dc9
LuigiBlood <luigiblood@gmail.com>
2020-06-23 12:55:05 +0200

    [DMPJ 1999-02-11] Do more 2D Paint Artwork Translation

--- 64DD/Mario Artist Paint Studio (1999-02-11 Proto)/ENG/DMPJ990211_US_2DPaint_Artworks.asm
+++ 64DD/Mario Artist Paint Studio (1999-02-11 Proto)/ENG/DMPJ990211_US_2DPaint_Artworks.asm
@@ -1,14 +1,49 @@
 // 2D Paint Artwork List
+//Internal Name / Screen Name
 
-//Nintendo Stamps
+//-Nintendo Stamps
+//Mario
+//Page 1
+putText2(0x1E3C5C+60*0, "Mario (1)")		//Mario 1 / マリオ(1)
+putText2(0x1E3C5C+60*1, "Mario (2)")		//Mario 2 / マリオ(2)
+putText2(0x1E3C5C+60*2, "Mario (3)")		//Mario 3 / マリオ(3)
+putText2(0x1E3C5C+60*3, "Mario (4)")		//Mario 4 / マリオ(4)
+putText2(0x1E3C5C+60*4, "Mario (5)")		//Mario 5 / マリオ(5)
+putText2(0x1E3C5C+60*5, "Mario (6)")		//Mario 6 / マリオ(6)
+putText2(0x1E3C5C+60*6, "Mario (7)")		//Mario 7 / マリオ(7)
 
-//Banjo & more
+//Page 2
+putText2(0x1E3C5C+60*7, "Mario (8)")		//Mario 8 / マリオ(8)
+putText2(0x1E3C5C+60*8, "Mario (9)")		//Mario 9 / マリオ(9)
+putText2(0x1E3C5C+60*9, "Mario (10)")		//Mario 10 / マリオ(10)
+putText2(0x1E3C5C+60*10, "Mario (11)")		//Mario 11 / マリオ(11)
+putText2(0x1E3C5C+60*11, "Mario (12)")		//Mario 12 / マリオ(12)
+putText2(0x1E3C5C+60*12, "Wing Cap Mario")	//Wing Cap Mario / はねマリオ
+putText2(0x1E3C5C+60*13, "Metal Mario")		//Metallic Mario / メタルマリオ
 
 //Page 3
-putText2(0x1E87D4, "Snorky")			//スノーキー
-putText2(0x1E8810, "Colon")				//コロン
-putText2(0x1E884C, "Gaburr")			//ガブール
-putText2(0x1E8888, "Filbert")			//フィルバート
-putText2(0x1E88C4, "Jack")				//ジャック
-putText2(0x1E8900, "Laura")				//ララ
-putText2(0x1E893C, "Linda")				//リンダ
+putText2(0x1E3C5C+60*14, "Toad")		//Toad / キノピオ
+putText2(0x1E3C5C+60*15, "Bowser (1)")		//Bowser 1 / クッパ(1)
+putText2(0x1E3C5C+60*16, "Bowser (2)")		//Bowser 2 / クッパ(2)
+putText2(0x1E3C5C+60*17, "Shiny Shell")		//Shiny Shell / カメのコウラ
+putText2(0x1E3C5C+60*18, "1UP Mushroom")	//1-up Mushroom / 1UPキノコ
+putText2(0x1E3C5C+60*19, "Star")		//Star / スター
+putText2(0x1E3C5C+60*20, "Red Block")		//Red Box / あかいブロック
+
+//Mario Kart
+//Page 1
+
+
+//Banjo & more
+//Page 1
+
+//Page 2
+
+//Page 3
+putText2(0x1E87D4, "Snorky")			//Snorky / スノーキー
+putText2(0x1E8810, "Colon")			//Sploog / コロン
+putText2(0x1E884C, "Gaburr")			//Gurble / ガブール
+putText2(0x1E8888, "Filbert")			//Filbert / フィルバート
+putText2(0x1E88C4, "Jack")			//Gloobey / ジャック
+putText2(0x1E8900, "Laura")			//Wizzle / ララ
+putText2(0x1E893C, "Linda")			//Lush / リンダ
