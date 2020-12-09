5a117b54425350cf37ef3467641ba2feb7fb41a2
LuigiBlood <luigiblood@gmail.com>
2020-12-09 15:56:28 +0100

    [Marvelous] Hack for easier team name controls

--- SNES/Marvelous/Main Game/asm/code.asm
+++ SNES/Marvelous/Main Game/asm/code.asm
@@ -32,4 +32,28 @@ asm_menu_fix:
 	clc
 	adc.w #$30A0
 	sta $7F009A
-	
+
+//Easier Team Naming Controls
+//B = Backspace
+//A = Enter Char
+//START = Begin
+//Odd = BYETUDLR, Even = AXLR...
+
+//$2C0F75 - $988F75 - START Button (don't touch)
+//$2C0FB1 - $988FB1 - L/R Buttons
+//$2C0FFE - $988FFE - Left/Right D-Pad (don't touch)
+//$2C1032 - $989032 - Up/Down D-Pad (don't touch)
+//$2C10A3 - $9890A3 - B Button
+//$2C10A9 - $9890A9 - X Button
+//$2C10B2 - $9890B2 - Y Button
+//$2C10C1 - $9890C1 - A Button (don't touch)
+//$7614 - Current Team Name Char
+//$9890E6 - Delete Current Char
+//$989157 - Go Back one char
+seekFile($2C10A3)
+	lda $F3
+	bit.b #$80
+	beq +
+	jsr $9157
+	jmp $90E6
++;	jmp $90C1
